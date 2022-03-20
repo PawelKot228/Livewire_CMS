a42ed2459c0b2d559b4d051ded6a31b62f299a88
PawelKot228 <p.kot@appgo.pl>
2022-03-20 02:34:13 +0100

    Admin api dark mode fix

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Validator;
 
 class IndexController extends Controller
 {
@@ -14,6 +16,42 @@ class IndexController extends Controller
     public function redirect()
     {
         return to_route('admin.index');
+    }
+
+    public function darkMode()
+    {
+        $dark_mode = request()->input('dark_mode', 1);
+
+        $validate = Validator::make(
+            ['dark_mode' => $dark_mode],
+            ['dark_mode' => 'required|int|digits_between:0,1']
+        );
+
+        if ($validate->fails()){
+            return [
+                'status' => __('admin.api.response.fail'),
+                'message' => $validate->getMessageBag(),
+            ];
+        }
+
+        DB::beginTransaction();
+        try {
+            $obj = \Auth::guard('admin')->user();
+            $obj->dark_mode = (int)$dark_mode;
+            $obj->save();
+
+            DB::commit();
+        } catch (\Exception){
+            DB::rollBack();
+            return [
+                'status' => __('admin.api.response.error'),
+                'message' => "Could not finish the task",
+            ];
+        }
+
+        return [
+            'status' => __('admin.api.response.success')
+        ];
     }
 
 }
