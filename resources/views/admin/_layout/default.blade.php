226b666785d4b4518c9ca5e24e5198bdf879f281
PawelKot228 <p.kot@appgo.pl>
2022-04-03 17:19:52 +0200

    visual fixes

--- resources/views/admin/_layout/default.blade.php
+++ resources/views/admin/_layout/default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Livewire CMS</title>
+    <title>{{config('app.name')}}</title>
 
     <link rel="stylesheet" href="{{asset('admin/css/vendors/adminlte.min.css')}}">
     <link rel="stylesheet" href="{{asset('admin/css/vendors/bootstrap4-toggle.min.css')}}">
@@ -121,15 +121,12 @@
 
         </ul>
     </nav>
-    <!-- /.navbar -->
 
-    <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
         <a href="{{route('admin.index')}}" class="brand-link">
             {{--            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"--}}
             {{--                 style="opacity: .8">--}}
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <span class="brand-text font-weight-light">{{config('app.name')}}</span>
         </a>
 
         <livewire:admin.admin-navigation/>
@@ -143,6 +140,7 @@
     </div>
 
     <footer class="main-footer">
+            &nbsp;
         <div class="float-right d-none d-sm-inline">
             <small>Currently using Laravel v{{App::VERSION()}}</small>
         </div>
